d736ed916755cee6eac62408f4b4dbb4cabb21c9
SaadUllah-cpu <67895759+SaadUllah-cpu@users.noreply.github.com>
2024-05-08 10:36:45 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all code as newest version
+
+// alert( 3+3) we are using node.js not a browser
+
+console.log(3+3)
+console.log("soun gye ooo")
+
+
+let name = "john"
+let age = 25
+let isLoggedIn = true
+
+// number => 2 to the power 52
+// bigint
+// boolean=> true/false
+// string=> ""
+// null=> standalone value
+// undefined => 
+// symbol=> unique
+// objects=> 
+    
+    
+    console.log(typeof null)
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 123454321
+let accountEmail = "none@none.com"
+var accountPaswrd = "12345"
+  accountCity = "Rajanpur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPaswrd, accountCity])
